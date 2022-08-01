33dbd7b46b0b90d3e736d58b02f1993d9e9f441e
Juraj <j.lormusz@gmail.com>
2022-08-01 16:09:31 +0200

    DB setup

---
+++ Ebay-project/Context/ApplicationContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ebay_project.Context
+{
+    public class ApplicationContext : DbContext
+    {
+    }
+}
--- Ebay-project/Program.cs
+++ Ebay-project/Program.cs
@@ -1,10 +1,27 @@
+using Ebay_project.Context;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 var logger = new LoggerConfiguration()
   .ReadFrom.Configuration(builder.Configuration)
   .Enrich.FromLogContext()
   .CreateLogger();
+
+if (env != null && env.Equals("Development"))
+{
+    builder.Services.AddDbContext<ApplicationContext>(dbBuilder => dbBuilder.UseSqlServer(builder.Configuration.GetConnectionString("LocalDB")));
+}
+if (env != null && env.Equals("Production"))
+{
+    var connectionString = builder.Configuration.GetConnectionString("AzureSql");
+    var sb = new SqlConnectionStringBuilder(connectionString);
+    sb.UserID = builder.Configuration["AzureUser"];
+    sb.Password = builder.Configuration["AzureSqlPassword"];
+    builder.Services.AddDbContext<ApplicationContext>(builder => builder.UseSqlServer(sb.ConnectionString));
+}
 
 // Add services to the container.
 builder.Logging.ClearProviders();
