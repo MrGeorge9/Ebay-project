761ae0fea257ba1ae618d79dc8cc1aaa88f66baf
Juraj <j.lormusz@gmail.com>
2022-08-01 15:50:16 +0200

    serilog settings

--- Ebay-project/Program.cs
+++ Ebay-project/Program.cs
@@ -1,6 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+var logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(builder.Configuration)
+  .Enrich.FromLogContext()
+  .CreateLogger();
 
 // Add services to the container.
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
