affad07ffee3c75a231298c02984337ab66896fa
Juraj <j.lormusz@gmail.com>
2022-08-04 16:45:20 +0200

    swagger and deployment preparation

--- Ebay-project/Program.cs
+++ Ebay-project/Program.cs
@@ -1,25 +1,31 @@
 using Ebay_project.Context;
 using Ebay_project.Exntensions;
-using Ebay_project.Models;
 using Ebay_project.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 Extensions.MapSecretsToEnvVariables();
-var logger = new LoggerConfiguration()
-  .ReadFrom.Configuration(builder.Configuration)
-  .CreateLogger();
 
 if (env != null && env.Equals("Development"))
 {
     builder.Services.AddDbContext<ApplicationContext>(dbBuilder => dbBuilder.UseSqlServer(builder.Configuration.GetConnectionString("LocalDB")));
+
+    var logger = new LoggerConfiguration()
+      .ReadFrom.Configuration(builder.Configuration)
+      .Enrich.FromLogContext()
+      .WriteTo.MSSqlServer(builder.Configuration.GetConnectionString("LocalDB"), autoCreateSqlTable: true, tableName: "Logs")
+      .CreateLogger();
+    builder.Logging.ClearProviders();
+    builder.Logging.AddSerilog(logger);
 }
+
 if (env != null && env.Equals("Production"))
 {
     var connectionString = builder.Configuration.GetConnectionString("AzureSql");
@@ -27,11 +33,17 @@ if (env != null && env.Equals("Production"))
     sb.UserID = builder.Configuration["AzureUser"];
     sb.Password = builder.Configuration["AzureSqlPassword"];
     builder.Services.AddDbContext<ApplicationContext>(builder => builder.UseSqlServer(sb.ConnectionString));
+
+    var logger = new LoggerConfiguration()
+     .ReadFrom.Configuration(builder.Configuration)
+     .Enrich.FromLogContext()
+     .WriteTo.MSSqlServer(sb.ConnectionString, autoCreateSqlTable: true, tableName: "Logs")
+     .CreateLogger();
+    builder.Logging.ClearProviders();
+    builder.Logging.AddSerilog(logger);
 }
 
 // Add services to the container.
-builder.Logging.ClearProviders();
-builder.Logging.AddSerilog(logger);
 builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -49,6 +61,14 @@ builder.Services.AddTransient<IBuyerService, BuyerService>();
 builder.Services.AddTransient<IAdminService, AdminService>();
 builder.Services.AddTransient<IAuthService, JwtService>();
 builder.Services.AddTransient<IPublicService, PublicService>();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Ebay API",
+        Description = "An ASP.NET Core Web API for online webshop Ebay project"
+    });
+});
 
 var app = builder.Build();
 
@@ -73,6 +93,15 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = String.Empty;
+});
+
 Extensions.FillDatabaseIfEmpty(app);
 
 app.Run();
+
+
